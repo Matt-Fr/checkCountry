912021ff794b1128cefc874c00addbdb221705c3
Matthieu Franck <matthieufranck@OrdinatMatthieu.lan>
2024-05-14 13:56:14 +0200

    add interface && get fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Country } from './models/country';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'checkCountry';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  posts: any = [];
+  ngOnInit(): void {
+    this.fetchCountries();
+  }
+  public fetchCountries() {
+    this.http
+      .get<Country[]>('https://restcountries.com/v3.1/alpha?codes=FRA,BEL,GER')
+      .subscribe((posts: any) => {
+        console.log(posts);
+      });
+  }
 }
